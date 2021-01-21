17e6a820b2853319c72d94662e170ab16ae77fc6
dariaoui <oussama.darioui@gmail.com>
2021-01-21 13:29:29 +0100

    first commit

---
+++ src/main/java/org/sid/springbootcrudmongodb/SpringbootCrudMongodbApplication.java
@@ -0,0 +1,13 @@
+package org.sid.springbootcrudmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCrudMongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCrudMongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/sid/springbootcrudmongodb/SpringbootCrudMongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sid.springbootcrudmongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCrudMongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
